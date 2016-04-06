08892d035702d8db64ff4e6e0f407394bdd8cc80
孙洪展 <sunhongzhan@msn.comgit config --global user.name Sun Hongzhangit config --global user.email sunhongzhan@msn.com>
2016-04-06 15:04:09 +0800

    第一次提交

---
+++ hello,word!/hello,word!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello,word!
+//
+//  Created by 孙洪展 on 16/4/6.
+//  Copyright © 2016年 孙洪展. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
